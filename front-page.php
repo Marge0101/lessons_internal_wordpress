282a27ea4bf47ffd8392cce3b2ebfdec5c17bbfd
Mari Sato <marge0101.ms@gmail.com>
2022-02-03 21:46:16 +0800

    added

--- front-page.php
+++ front-page.php
@@ -15,14 +15,15 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main hero_page">
+<main id="primary" class="site-main hero_page">
 
-	<div class="hero_text">
-		<h1>Xeory Extension</h1>
-		<p>Xeory Extensionはワンコラムのサイト型テーマです。ユーザーに対して重要なコンテンツを効率的に届けられるようにデザインされています。５種類のカラーバリエーションを用意しています。</p>
-	</div>
-		
-	</main><!-- #main -->
+    <div class="hero_text">
+        <h1>Xeory Extension</h1>
+        <p>Xeory Extensionはワンコラムのサイト型テーマです。ユーザーに対して重要なコンテンツを効率的に届けられるようにデザインされています。５種類のカラーバリエーションを用意しています。</p>
+    </div>
+
+</main><!-- #main -->
+<h1>test</h1>
 
 <?php 
 get_sidebar();
